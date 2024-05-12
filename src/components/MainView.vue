91d37a13b7c91843b741d85deaa60b9ff64b8879
soljjang777 <vivace8520vi@naver.com>
2024-05-12 21:13:32 +0900

    폰트 추가

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -20,8 +20,13 @@ export default {
     }
     const zz = ref("");
 
+
     const handleScroll = () => {
       console.log(zz.value);
+    };
+
+    const textOverlayPosition = (index) => {
+      return index === photos.length - 1 ? "100px" : "40px";
     };
 
     onMounted(() => {
@@ -36,6 +41,7 @@ export default {
     return {
       photos,
       onClickMoveDetailPage,
+      textOverlayPosition,
     };
   }
 }
@@ -47,12 +53,21 @@ export default {
       <div class="photo-wrap" v-for="(photo, index) in photos" :key="index">
         <div class="photo-flame" @click="onClickMoveDetailPage(index)">
           <img :src="photo.src[0]">
-          <div class="text-overlay">
-            <p>{{ photo.type }}</p>
-            <p>{{ photo.name }}</p>
-            <p>{{ photo.clientType }}.{{ photo.client }}</p>
-            <p>Place.{{photo.place}}</p>
-            <p>{{ photo.date === "" ? "unreleased" : `Date.${photo.date}`}}</p>
+          <div class="text-overlay" :style="{ bottom: textOverlayPosition(index) }">
+            <div class="text-middle">{{ photo.type }}</div>
+            <div class="text-big">{{ photo.name }}</div>
+            <div class="text-small">
+              <b class="light">{{ photo.clientType }}.</b>
+              <b class="bold">{{ photo.client }}</b>
+            </div>
+            <div class="text-small">
+              <b class="light">Place.</b>
+              <b class="bold">{{photo.place}}</b>
+            </div>
+            <div class="text-small">
+              <b class="light">{{ photo.date === "" ? "" : `Date.`}}</b>
+              <b class="bold">{{ photo.date === "" ? "unreleased" : `${photo.date}`}}</b>
+            </div>
           </div>
         </div>
       </div>
@@ -67,5 +82,58 @@ export default {
 </template>
 
 <style scoped>
+/* 메인 콘텐츠 스타일링 */
+main {
 
+}
+
+section {
+
+}
+
+.photo-flame {
+  position: relative; /* 포지션 설정 */
+  max-width: 100%;
+  overflow: hidden; /* 이미지가 부모 요소를 넘어가지 않도록 합니다 */
+}
+
+.photo-flame img {
+  width: 100%; /* 이미지의 너비를 부모 요소에 맞게 100%로 설정합니다 */
+  height: auto; /* 이미지의 가로 세로 비율을 유지합니다 */
+  display: block; /* 인라인 요소에서 블록 요소로 변환하여 가로 너비를 100%로 설정합니다 */
+}
+
+.text-overlay {
+  position: absolute; /* 포지션 설정 */
+  color: #fff; /* 텍스트 색상을 흰색으로 설정 */
+  transition: bottom 0.3s ease; /* 스크롤 시 bottom 속성을 부드럽게 변경 */
+}
+.text-overlay .text-middle {
+  font-family: GmarketSansTTFBold;
+}
+
+.text-overlay .text-big {
+  font-family: Sandoll CinemaTheater;
+  font-weight: 400;
+}
+
+.text-overlay .text-small .light{
+  font-family: GmarketSansTTFLight;
+}
+
+.text-overlay .text-small .bold{
+  font-family: GmarketSansTTFBold;
+}
+
+.photo-line {
+  display: flex; /* Flexbox 사용 */
+  justify-content: center; /* 가로 방향으로 가운데 정렬 */
+  align-items: center; /* 세로 방향으로 가운데 정렬 */
+  height: 100%; /* 높이 설정 */
+}
+
+.photo-line img {
+  max-width: 100%; /* 이미지 너비 최대 크기로 설정 */
+  height: auto; /* 이미지의 가로 세로 비율 유지 */
+}
 </style>
