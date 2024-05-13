f6707d0e8d6185eb31370bec1a4513f9bbb181bb
soljjang777 <vivace8520vi@naver.com>
2024-05-13 23:37:23 +0900

    글씨 이동 되도록 수정

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,6 +1,6 @@
 <script>
 import {useRouter} from "vue-router";
-import { getCurrentInstance, onMounted, onBeforeUnmount, ref } from "vue";
+import { getCurrentInstance } from "vue";
 import Route from "@/router/route"
 
 export default {
@@ -19,35 +19,6 @@ export default {
       })
     }
 
-    let idx = ref(0);
-
-    const handleScroll = () => {
-      let position = 50;
-      const photoFlames = document.querySelectorAll('.photo-flame');
-      const textOverlays = document.querySelectorAll('.text-overlay');
-
-      const photoFlameRect = photoFlames[idx.value].getBoundingClientRect();
-      const textOverlaysRect = textOverlays[idx.value].getBoundingClientRect();
-      console.log(textOverlaysRect.bottom)
-      // console.log(photoFlameRect.bottom);
-      if (photoFlameRect.top <= 0) {
-        position = window.scrollY;
-      }
-
-      textOverlays[idx.value].style.top = `${position}px`;
-
-
-    };
-
-    onMounted(() => {
-      // 스크롤 이벤트 리스너 추가
-      window.addEventListener('scroll', handleScroll);
-    });
-
-    onBeforeUnmount(() => {
-      // 컴포넌트가 제거되기 전에 스크롤 이벤트 리스너 제거
-      window.removeEventListener('scroll', handleScroll);
-    });
     return {
       photos,
       onClickMoveDetailPage,
@@ -62,27 +33,27 @@ export default {
       <div class="photo-wrap" v-for="(photo, index) in photos" :key="index">
         <div class="photo-flame" @click="onClickMoveDetailPage(index)">
           <img :src="photo.src[0]">
-          <div class="text-overlay">
-            <div class="text-middle">{{ photo.type }}</div>
-            <div class="text-big">{{ photo.name }}</div>
-            <div class="text-small">
-              <b class="light">{{ photo.clientType }}. </b>
-              <b class="bold">{{ photo.client }}</b>
-            </div>
-            <div class="text-small">
-              <b class="light">Place. </b>
-              <b class="bold">{{photo.place}}</b>
-            </div>
-            <div class="text-small">
-              <b class="light">{{ photo.date === "" ? "" : `Date. `}}</b>
-              <b class="bold">{{ photo.date === "" ? "unreleased" : `${photo.date}`}}</b>
+          <div class="text-overlay-wrap">
+            <div class="text-overlay">
+              <div class="text-middle">{{ photo.type }}</div>
+              <div class="text-big">{{ photo.name }}</div>
+              <div class="text-small">
+                <b class="light">{{ photo.clientType }}. </b>
+                <b class="bold">{{ photo.client }}</b>
+              </div>
+              <div class="text-small">
+                <b class="light">Place. </b>
+                <b class="bold">{{photo.place}}</b>
+              </div>
+              <div class="text-small">
+                <b class="light">{{ photo.date === "" ? "" : `Date. `}}</b>
+                <b class="bold">{{ photo.date === "" ? "unreleased" : `${photo.date}`}}</b>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="photo-line">
-        <img src="@/assets/images/bottomLineLogo.png">
-      </div>
+      <img src="@/assets/images/bottomLineLogo.png" style="width: 100%;">
       <div class="photo-flame">
         <img src="../assets/images/myPhoto.jpg">
       </div>
